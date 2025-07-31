110d3add0615796c8834e9d6582337b54b54f1ab
Kuanishbayev <81415506+Kuanishbayev@users.noreply.github.com>
2025-07-31 22:59:46 +0500

    User model changed

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    // comment delete me
+
     /**
      * The attributes that are mass assignable.
      *
