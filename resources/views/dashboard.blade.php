b3429527785d1b998ac57661b9e93098e7d24300
Kuanishbayev <81415506+Kuanishbayev@users.noreply.github.com>
2025-08-03 22:01:53 +0500

    Functionality of Application forms and Application lists are done.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,75 +11,50 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @if (auth()->user()->role_id === 1)
                     <span class="text-blue-500 font-bold text-xl">Received applications</span>
-                        <div>
-                            <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white mt-5">
-                                <div class="flex w-full items-center justify-between border-b pb-3">
-                                <div class="flex items-center space-x-3">
-                                    <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
-                                    <div class="text-lg font-bold text-slate-700">Joe Smith</div>
-                                </div>
-                                <div class="flex items-center space-x-8">
-                                    <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold text-gray-500">Category</button>
-                                    <div class="text-xs text-neutral-500">2 hours ago</div>
-                                </div>
-                                </div>
+                        @foreach ($applications as $application)
+                            <div>
+                                <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white mt-5">
+                                    <div class="flex w-full items-center justify-between border-b pb-3">
+                                    <div class="flex items-center space-x-3">
+                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
+                                        <div class="text-lg font-bold text-slate-700">{{ $application->user->name }}</div>
+                                    </div>
+                                    <div class="flex items-center space-x-8">
+                                        <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold text-gray-500">id: {{ $application->id }}</button>
+                                        <div class="text-xs text-neutral-500">{{ $application->created_at }}</div>
+                                    </div>
+                                    </div>
 
-                                <div class="mt-4 mb-6">
-                                <div class="mb-3 text-xl font-bold text-black">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
-                                <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
-                                </div>
+                                    <div class="mt-4 mb-6">
+                                    <div class="mb-3 text-xl font-bold text-black">{{ $application->subject }}</div>
+                                    <div class="text-sm text-neutral-600">{{ $application->message }}</div>
+                                    </div>
 
-                                <div>
-                                <div class="flex items-center justify-between text-slate-500">
-                                    <div class="flex space-x-4 md:space-x-8">
-                                        john@gmail.com
+                                    <div>
+                                    <div class="flex items-center justify-between text-slate-500">
+                                        <div class="flex space-x-4 md:space-x-8">
+                                            {{ $application->user->email }}
+                                        </div>
+                                    </div>
                                     </div>
                                 </div>
-                                </div>
                             </div>
-                        </div>
-                        <div>
-                            <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white mt-5">
-                                <div class="flex w-full items-center justify-between border-b pb-3">
-                                <div class="flex items-center space-x-3">
-                                    <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]"></div>
-                                    <div class="text-lg font-bold text-slate-700">Joe Smith</div>
-                                </div>
-                                <div class="flex items-center space-x-8">
-                                    <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold text-gray-500">Category</button>
-                                    <div class="text-xs text-neutral-500">2 hours ago</div>
-                                </div>
-                                </div>
-
-                                <div class="mt-4 mb-6">
-                                <div class="mb-3 text-xl font-bold text-black">Nulla sed leo tempus, feugiat velit vel, rhoncus neque?</div>
-                                <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna. Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec. Curabitur eget feugiat massa</div>
-                                </div>
-
-                                <div>
-                                <div class="flex items-center justify-between text-slate-500">
-                                    <div class="flex space-x-4 md:space-x-8">
-                                        john@gmail.com
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     @else
                         <div class='flex items-center'>
                             <div class='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
                                 <div class='max-w-md mx-auto space-y-6'>
 
-                                    <form action="" method="POST" enctype="multipart/form-data">
+                                    <form action="{{ route('applications.store') }}" method="POST" enctype="multipart/form-data">
                                         @csrf
                                         <h2 class="text-2xl font-bold ">Submit your application</h2>
                                         <hr class="my-6">
                                         <label class="uppercase text-sm font-bold opacity-70">Subject</label>
-                                        <input type="text" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <input type="text" name="subject" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none" required>
                                         <label class="uppercase text-sm font-bold opacity-70">Message</label>
-                                        <textarea name="" id="" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded" rows="5"></textarea>
+                                        <textarea name="message" id="" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded" rows="5" required></textarea>
                                         <label class="uppercase text-sm font-bold opacity-70">File</label>
-                                        <input type="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <input type="file" name="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
                                         <input type="submit" class="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300" value="Send">
                                     </form>
 
@@ -87,6 +62,9 @@
                             </div>
                         </div>
                     @endif
+                    <div class="mt-5">
+                        {{ $applications->links() }}
+                    </div>
                 </div>
             </div>
         </div>
